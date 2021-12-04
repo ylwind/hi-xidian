4a8fb889aa1a155faeb5327de24a8a2640b1ff3d
sbzhang <sbzhang@ylwind.cn>
2021-12-04 23:52:02 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-hi-xidian
+![hello-nav](https://socialify.git.ci/hello-nav/hello-nav/image?description=1&descriptionEditable=A%20navigation%20for%20developer&font=Raleway&logo=https%3A%2F%2Fhello-nav.github.io%2Ffavicon%2Ffavicon-192.png&pattern=Plus&theme=Light)
+
+## Hello Nav' website
+
+[https://hello-nav.github.io](https://hello-nav.github.io)
+
+## 🗺 Features
+
+- 160+ Apps
+- App search with keywords
+- List or category view
+- Github link
+- Offline
+- Dark mode
+
+## 🎨 Design
+
+![hello-nav](https://raw.githubusercontent.com/hello-nav/hello-nav/master/doc/images/hello-nav.png)
+
+![hello-nav-search](https://raw.githubusercontent.com/hello-nav/hello-nav/master/doc/images/hello-nav-search.png)
+
+![hello-nav-dark](https://github.com/hello-nav/hello-nav/raw/master/doc/images/hello-nav-dark.png)
+
+## 📄 License
+
+Hello Nav is [MIT](https://github.com/hello-nav/hello-nav/blob/master/LICENSE) license.
+
+© 2018-present Bener
---
+++ precache-manifest.2ee69339ea619e50b1e09e65eb4ffdda.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9cc354a27c85a5a0e179ec3f754d65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ec28c5dfe4018a11498",
+    "url": "/static/js/2.d09b6a84.chunk.js"
+  },
+  {
+    "revision": "18bf94db2b483f105b1b",
+    "url": "/static/js/main.5f68f1c2.chunk.js"
+  },
+  {
+    "revision": "7afb10e8d62a811a23cb",
+    "url": "/static/js/runtime~main.46b21fbf.js"
+  },
+  {
+    "revision": "2100edafbd1cab6b3564be1fdcc7c119",
+    "url": "/static/media/bi.2100edaf.woff"
+  },
+  {
+    "revision": "5b504fca88a64d9e187a483a9c7af16a",
+    "url": "/static/media/bi.5b504fca.ttf"
+  },
+  {
+    "revision": "bca6cea1e5014c8911f154f8f12cc132",
+    "url": "/static/media/oa.bca6cea1.png"
+  },
+  {
+    "revision": "4d5b89395996af41fa690d114fcf843a",
+    "url": "/static/media/xdu.4d5b8939.png"
+  },
+  {
+    "revision": "6c3c8628e8ef209482fe4f6c5d69864f",
+    "url": "/static/media/xdu.6c3c8628.jpg"
+  }
+]);
